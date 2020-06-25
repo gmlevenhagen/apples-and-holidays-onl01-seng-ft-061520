67a1b3eb2c44dde8f3f45c4bac68e90248ce8645
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-25 21:19:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,13 +55,12 @@ array.flatten# return an array of all of the supplies that are used in the winte
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, data|
-    puts "#{season.to_s.capitalize!}:"
-    data.each do |holiday, supply|
-      array = holiday.to_s.split("_")
-      final_holiday = []
-        array.each do |x|
-          final_holiday << x.capitalize!
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! << ":"
+    holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ") << ":"
+      puts "  " + holiday + " " + supplies
   end
 end
 end
